1a58584ab4500696051b9102455ab767d7532d35
Chris Gamberg <cgamberg@gmail.com>
2020-01-19 15:20:30 -0500

    Added DB Context and ConnStr

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ARIA_Net.Data;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace ARIA_Net
 {
@@ -24,6 +27,9 @@ namespace ARIA_Net
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContext<ScoreContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("ScoreContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
